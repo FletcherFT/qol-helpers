7123d211b49667892ad7bee50f1996e430b0dc0a
FFT <fletcherthompsonx@gmail.com>
2023-02-15 13:07:34 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='qol-helpers',
+    author='Fletcher F Thompson',
+    author_email='fletcherthompsonx@gmail.com.com',
+    url="https://github.com/FletcherFT/qol-helpers/",
+    version='0.0.1',
+    install_requires=[
+    ],
+    packages=find_packages()
+)
