b17d38058635f824c5603813502aac5809f98f75
FFT <fletho@dtu.dk>
2023-04-14 20:32:45 +0200

    Multithreaded find_files function

--- qolhelpers/utils.py
+++ qolhelpers/utils.py
@@ -10,33 +10,42 @@ import json
 from multiprocessing.pool import ThreadPool
 import itertools
 import tqdm
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from typing import List, Union, Generator
 
 
-def find_files(extension: str, paths: List[Path], recursive: bool = False) -> List[Path]:
+def find_files_worker(extension: str, path: Path, recursive: bool) -> List[Path]:
+    if not extension.startswith("."):
+        extension = "." + extension
+    if path.is_dir():
+        return list(path.rglob(f"*{extension}")) if recursive else list(path.glob(f"*{extension}"))
+    else:
+        return []
+
+
+def find_files(extension: str, paths: List[Path], recursive: bool = False, num_threads: int = 4) -> List[Path]:
     """
-    Finds files with the specified extension in the specified paths and returns them as  a list.
-    :param extension: The extension of the file, starts with a '.' (e.g. '.png')
-    :param paths:  A list of Path objects pointing to specific files, or directories to search.
-    :param recursive: Flag to indicate if the directory search should be recursive or not.
-    :return: A list of valid Path objects.
+        Finds files with the specified extension in the specified paths and returns them as  a list.
+        :param extension: The extension of the file, starts with a '.' (e.g. '.png')
+        :param paths:  A list of Path objects pointing to specific files, or directories to search.
+        :param recursive: Flag to indicate if the directory search should be recursive or not.
+        :param num_threads: Number of worker threads to complete search.
+        :return: A list of valid Path objects.
     """
     if not extension.startswith("."):
         extension = "." + extension
     file_paths = []
-    for path in paths:
-        if not path.exists():
-            print(f"Could not find {path}.")
-            continue
-        if path.is_dir():
-            file_paths.extend(path.rglob(f"*{extension}")) if recursive else file_paths.extend(path.glob(f"*{extension}"))
-        elif path.is_file():
-            if path.suffix != extension:
-                print(f"Not a {extension} file: {path}.")
-                continue
-            file_paths.append(path)
-        else:
-            print(f"Not a file or directory: {path}")
-            continue
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        tasks = [executor.submit(find_files_worker, extension, path, recursive) for path in paths]
+        for task in tasks:
+            try:
+                result = task.result()
+                file_paths.extend(result)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
     return file_paths
 
 
